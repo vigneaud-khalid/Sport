326d51ab9be614179e3043e9ea2fa96eae452a21
IIOhanaII <barbey.arthur@gmail.com>
2021-05-25 17:30:46 +0200

    [AB] Navbar & Page d'accueil

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // localhost:4200/
+  { path: 'home', component: HomeComponent },
+  // pathMatch = "full" signifie que tout chemin d'url doit correspondre
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  // On affichera error.component en cas de chemin inexistant
+  { path: '**', redirectTo: '/error' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {onSameUrlNavigation: 'reload'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
