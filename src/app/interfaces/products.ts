839693d3a13a8a66b44256d3b69dd46d5e0009f1
BALLEREAU <emmanuel.ballereau@hotmail.fr>
2021-05-25 16:27:33 +0200

    [EB] products.ts

---
+++ src/app/interfaces/products.ts
@@ -0,0 +1,9 @@
+export interface Products {
+    idProduct: number;
+    name: string;
+    description: string;
+    price: number;
+    Category: number;
+    stock: number;
+    image: string;
+}
