b9f8c3c105d8e115de01eef44a45f6dc77837d27
BALLEREAU <emmanuel.ballereau@hotmail.fr>
2021-05-25 18:12:39 +0200

    [EB] Creation des composants Angular

--- src/app/interfaces/products.ts
+++ src/app/interfaces/products.ts
@@ -1,9 +1,9 @@
 export interface Products {
-    idProduct: number;
-    name: string;
-    description: string;
-    price: number;
-    Category: number;
-    stock: number;
-    image: string;
+    idProduct?: number;
+    name?: string;
+    description?: string;
+    price?: number;
+    Category?: number;
+    stock?: number;
+    image?: string;
 }
--- src/app/interfaces/users.ts
+++ src/app/interfaces/users.ts
@@ -1,2 +1,9 @@
 export interface Users {
+    idUser?: number;
+    firstname?: string;
+    lastname?: string;
+    email?: string;
+    password?: string;
+    address?: string;
+      
 }
