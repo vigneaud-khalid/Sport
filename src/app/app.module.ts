04f6d41d4b1a269fbd8c4742eda7e70e81b7a146
IIOhanaII <barbey.arthur@gmail.com>
2021-05-26 15:32:56 +0200

    [AB] Page d'accueil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,14 @@
 import { NgModule } from '@angular/core';
-// import { BrowserModule } from '@angular/platform-browser';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
-import { ContactComponent } from './contact/contact.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { ProductsComponent } from './market/products/products.component';
-import { SingleProductComponent } from './market/single-product/single-product.component';
-import { CartComponent } from './market/cart/cart.component';
-import { MarketComponent } from './composants/market/market.component';
-import { ProductComponent } from './composants/product/product.component';
+import { HomeComponent } from './composants/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    HomeComponent,
-    ContactComponent,
-    LoginComponent,
-    RegisterComponent,
-    ProductsComponent,
-    SingleProductComponent,
-    CartComponent,
-    MarketComponent,
-    ProductComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
