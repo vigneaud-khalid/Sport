97dbbacf319f8b486eb1f81b3ef3ca42b487d6f7
AmadouTidiane2021 <tidiane79@yahoo.fr>
2021-05-26 16:53:05 +0200

    atd modif

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './composants/home/home.component';
+import { ReservationComponent } from './composants/reservation/reservation.component';
+
 
 const routes: Routes = [
     // localhost:4200/
     { path: 'home', component: HomeComponent },
+    // localhost:4200/reservation 
+    { path: 'reservation', component: ReservationComponent },
     // pathMatch = "full" signifie que tout chemin d'url doit correspondre
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     // On affichera error.component en cas de chemin inexistant
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,30 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
-import { ContactComponent } from './contact/contact.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { ProductsComponent } from './market/products/products.component';
-import { SingleProductComponent } from './market/single-product/single-product.component';
-import { CartComponent } from './market/cart/cart.component';
 import { MarketComponent } from './composants/market/market.component';
 import { ProductComponent } from './composants/product/product.component';
+import { ReservationComponent } from './composants/reservation/reservation.component';
+import { HomeComponent } from './composants/home/home.component';
+import { ContactComponent } from './composants/contact/contact.component';
+import { LoginComponent } from './composants/login/login.component';
+import { CartComponent } from './composants/cart/cart.component';
+import { RegisterComponent } from './composants/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     HomeComponent,
     ContactComponent,
-    LoginComponent,
-    RegisterComponent,
-    ProductsComponent,
-    SingleProductComponent,
+   LoginComponent,
+   RegisterComponent,
     CartComponent,
     MarketComponent,
-    ProductComponent
+    ProductComponent,
+    ReservationComponent
   ],
   imports: [
     BrowserModule,
