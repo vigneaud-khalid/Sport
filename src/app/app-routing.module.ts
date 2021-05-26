5ea3ce8e359142ac1bd60b0ff6888e70ea5613ad
IIOhanaII <barbey.arthur@gmail.com>
2021-05-26 18:03:47 +0200

    Routing & fake API data

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './composants/home/home.component';
+import { MarketComponent } from './composants/market/market.component';
+import { ProductComponent } from './composants/product/product.component';
+import { ReservationComponent } from './composants/reservation/reservation.component';
+import { ContactComponent } from './composants/contact/contact.component';
+import { LoginComponent } from './composants/login/login.component';
+import { CartComponent } from './composants/cart/cart.component';
+import { RegisterComponent } from './composants/register/register.component';
+import { ErrorComponent } from './composants/error/error.component';
+
 
 const routes: Routes = [
     // localhost:4200/
     { path: 'home', component: HomeComponent },
+    // localhost:4200/market
+    { path: 'market', component: MarketComponent },
+    // localhost:4200/product
+    { path: 'product', component: ProductComponent },
+    // localhost:4200/reservation
+    { path: 'reservation', component: ReservationComponent },
+    // localhost:4200/contact
+    { path: 'contact', component: ContactComponent },
+    // localhost:4200/login
+    { path: 'login', component: LoginComponent },
+    // localhost:4200/cart
+    { path: 'cart', component: CartComponent },
+    // localhost:4200/register
+    { path: 'register', component: RegisterComponent },
+    // localhost:4200/error
+    { path: 'error', component: ErrorComponent },
     // pathMatch = "full" signifie que tout chemin d'url doit correspondre
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     // On affichera error.component en cas de chemin inexistant
@@ -12,7 +37,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {onSameUrlNavigation: 'reload'})],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MarketComponent } from './composants/market/market.component';
+import { ProductComponent } from './composants/product/product.component';
+import { ReservationComponent } from './composants/reservation/reservation.component';
 import { HomeComponent } from './composants/home/home.component';
+import { ContactComponent } from './composants/contact/contact.component';
+import { LoginComponent } from './composants/login/login.component';
+import { CartComponent } from './composants/cart/cart.component';
+import { RegisterComponent } from './composants/register/register.component';
+import { ErrorComponent } from './composants/error/error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ContactComponent,
+    LoginComponent,
+    RegisterComponent,
+    CartComponent,
+    MarketComponent,
+    ProductComponent,
+    ReservationComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
