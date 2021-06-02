68e39130736fd5e696c8dd91fa613bcb719d6e63
vigneaud-khalid <khalid40@yahoo.fr>
2021-06-02 16:53:47 +0200

    modifs on db

--- src/app/shared/token-storage.service.ts
+++ src/app/shared/token-storage.service.ts
@@ -25,17 +25,11 @@ export class TokenStorageService {
   saveUser(user: User): void {
     sessionStorage.removeItem(USER_KEY);
     sessionStorage.setItem(USER_KEY, JSON.stringify(user));
-    //sessionStorage.setItem(ROLE_KEY, JSON.stringify(user.idUser));
   }
 
   saveRole(role: any): void {
-    console.log("roleeee!!!! = " +role);
-    console.log('TOKENSTORAGE_saveRole_0');
     sessionStorage.removeItem('rolekey');
     sessionStorage.setItem('rolekey', role);
-    let nomRole = sessionStorage.getItem('rolekey');
-    console.log('Role(role) = '+nomRole);
-    console.log('Role(ROLE_KEY) = '+rolekey);
     this.checkAdmin();
   }
   getUser(): any {
@@ -54,8 +48,6 @@ export class TokenStorageService {
   checkAdmin(): any {
     let nomRole = sessionStorage.getItem('rolekey');
     if (nomRole=="admin") {
-      console.log('checkAdmin_2');
-      console.log('Role(role) = '+nomRole);
       return true
     }
     console.log('checkAdmin_3 false');
